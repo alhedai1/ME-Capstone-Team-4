38dff82de3019be5c6ec3f212aa0c2d235192554
Ahmed Mohamed <ahmedalhidai@outlook.com>
2026-05-23 12:19:36 +0900

    aa

--- src/capstone_robot/testing/test_motor.py
+++ src/capstone_robot/testing/test_motor.py
@@ -1,7 +1,7 @@
 from gpiozero import Robot
 from time import sleep
 
-robot = Robot(left=('BOARD32', 'BOARD33'), right=('BOARD35', 'BOARD12'))
-robot.value = (0.1, 0.1)
-sleep(2)
+robot = Robot(left=('BOARD32', 'BOARD33'), right=('BOARD12', 'BOARD35'))
+robot.value = (0.5, 0)
+sleep(5)
 robot.stop()
